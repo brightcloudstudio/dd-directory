1a7be9001f0be362730cb59c712e09804739241a
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-05-01 14:43:24 -0400

    Configure backend and frontend modules in config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,4 +1,23 @@
 <?php
 
-/* Hooks */
-//$GLOBALS
+use Contao\System;
+
+/* Back end modules */
+$GLOBALS['BE_MOD']['content']['listings'] = array(
+	'tables' => array('tl_listing'),
+	'icon'   => 'system/modules/contao_directory/assets/icons/location.png',
+	'exportListings' => array('Bcs\Backend\ListingsBackend', 'exportListings')
+);
+
+/* Front end modules */
+$GLOBALS['FE_MOD']['contao_directory']['directory_list'] 	= 'Bcs\Module\DirectoryList';
+
+/* Models */
+$GLOBALS['TL_MODELS']['tl_listing'] = 'Bcs\Model\Listing';
+
+/* Add Backend CSS to style Reviewed and Unreviewed */
+$request = System::getContainer()->get('request_stack')->getCurrentRequest();
+if ($request && System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest($request))
+{
+	$GLOBALS['TL_CSS'][]					= 'system/modules/contao_directory/assets/css/contao_directory_backend.css';
+}
