9d7a6b2f445aea6d70d67231fa55b2612973b3e0
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-05-04 12:20:48 -0400

    Create modules.php

---
+++ src/Resources/contao/languages/en/modules.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Contao Directory - Users can make submissions to a directory with a module to display and filter the results.
+ *
+ * Copyright (C) 2022 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/contao-directory
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+/* Back end modules */
+$GLOBALS['TL_LANG']['MOD']['listings']          = array('Listings', 'Listings for the Directory');
+
+/* Front end modules */
+$GLOBALS['TL_LANG']['FMD']['directory_list']    = array('Directory List', 'Displays Listings and the filters to customize them');
