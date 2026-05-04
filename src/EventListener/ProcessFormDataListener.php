9fa62622151d5952f4414351a264cb8f509ea643
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-05-04 12:07:08 -0400

    Update ProcessFormDataListener.php

--- src/EventListener/ProcessFormDataListener.php
+++ src/EventListener/ProcessFormDataListener.php
@@ -7,8 +7,13 @@
     
 namespace Bcs\DDDirectoryBundle\EventListener;
 
+use Bcs\Model\Listing;
+
 use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
+use Contao\Database;
+use Contao\FilesModel;
 use Contao\Form;
+use Contao\StringUtil;
 
 #[AsHook('processFormData')]
 class ProcessFormDataListener
@@ -23,7 +28,181 @@ class ProcessFormDataListener
 
     public function __invoke(array $submittedData, array $formData, ?array $files, array $labels, Form $form): void
     {
-        echo "HOOKED!";
-        die();
+        // If our new conditional form is being submitted
+        if($formData['formID'] == 'directory_submission_conditional') {
+            
+            // Create new Listing
+            $listing = new Listing();
+            $listing->tstamp = time();
+            $listing->date_created = time();
+            $listing->published = 1;
+            $listing->approved = 'unapproved';
+            
+            // Your Information
+            $listing->first_name = $submittedData['first_name'];
+            $listing->last_name = $submittedData['last_name'];
+            $listing->credentials = $submittedData['credentials'];
+            $listing->phone = $submittedData['phone'];
+            $listing->email_internal = $submittedData['email_internal'];
+            $listing->email_public = $submittedData['email_public'];
+            $listing->website = $submittedData['website'];
+            
+            // Photo from upload field
+            $img = FilesModel::findByUuid($files['photo']['uuid']);
+            $bin = StringUtil::uuidToBin($files['photo']['uuid']);
+            $listing->photo = $bin;
+            
+            $listing->address_1 = $submittedData['address_1'];
+            $listing->address_2 = $submittedData['address_2'];
+            $listing->country = $submittedData['country'];
+            
+            if($submittedData['country'] == 'Canada')
+                $listing->state = $submittedData['providence'];
+            else if($submittedData['country'] == 'USA')
+                $listing->state = $submittedData['state'];
+            else
+                $listing->state = "N/A";
+            
+            $listing->city = $submittedData['city'];
+            $listing->zip = $submittedData['zip'];
+            $listing->how_to_contact = $submittedData['how_to_contact'];
+            
+            // Service Area
+            $listing->service_area_worldwide = $submittedData['service_area_worldwide'];
+            $listing->service_area_country = $submittedData['service_area_country'];
+            $listing->service_area_state = $submittedData['service_area_state'];
+            $listing->service_area_province = $submittedData['service_area_province'];
+            
+            // Profession
+            $listing->profession = $submittedData['profession'];
+            
+            // Specialties
+            $listing->specialties_1 = $submittedData['specialties_1'];
+            $listing->specialties_2 = $submittedData['specialties_2'];
+            $listing->specialties_3 = $submittedData['specialties_3'];
+            $listing->specialties_4 = $submittedData['specialties_4'];
+            
+            // Which language
+            $listing->language = $submittedData['language'];
+            
+            // Description of practice
+            $listing->describe_practice = $submittedData['describe_practice'];
+            
+            // Yes/No
+            $listing->remote_consultations = $submittedData['remote_consultations'];
+            $listing->provide_mms = $submittedData['provide_mms'];
+            $listing->provide_cas = $submittedData['provide_cas'];
+            $listing->training_program = $submittedData['training_program'];
+            
+            // Specific Clinical Services
+            $listing->specific_services = $submittedData['specific_services'];
+            
+            // Save the new Listing
+            $listing->save();
+            
+            // Create a log of this submission
+            $myfile = fopen($_SERVER['DOCUMENT_ROOT'] . '/../files/content/directory/logs/directory_submission_'.strtolower(date('m_d_y_H:m:s')).".txt", "w") or die("Unable to open file!");
+            fwrite($myfile, "Listing ID: " . $listing->id . "\n");
+            foreach($submittedData as $key => $val) {
+                fwrite($myfile, "Key: " . $key . "  | Value: " . $val . "\n");
+            }
+            fclose($myfile);
+
+            // Setup strings for our email
+            $service_area_worldwide = $submittedData['service_area_worldwide'];
+            
+            $service_area_country = '';
+            if(is_array($submittedData['service_area_country'])) {
+                $service_area_country = implode(',', $submittedData['service_area_country']);
+            } else {
+                $service_area_country = $submittedData['service_area_country'];
+            }
+            
+            $service_area_state = '';
+            if(is_array($submittedData['service_area_state'])) {
+                $service_area_state = implode(',', $submittedData['service_area_state']);
+            } else {
+                $service_area_state = $submittedData['service_area_state'];
+            }
+            
+            $service_area_province = '';
+            if(is_array($submittedData['service_area_province'])) {
+                $service_area_province = implode(',', $submittedData['service_area_province']);
+            } else {
+                $service_area_province = $submittedData['service_area_province'];
+            }
+
+            // Create our email message
+            $message_start = "
+            <html>
+            <head>
+            <title>[DD] New Directory Submission</title>
+            </head>
+            <body>
+            <h2>Listing Details:</h2>
+            ";
+            $message_user_contents = "";
+            $message_end = "
+            </body>
+            </html>
+            ";
+
+            // build the html for the users contents
+            $message_user_contents = $message_user_contents . '<p>First Name: '.$submittedData['first_name'].'</p>'. "\r\n";
+            $message_user_contents = $message_user_contents . '<p>Last Name: '.$submittedData['last_name'].'</p>'. "\r\n";
+            $message_user_contents = $message_user_contents . '<p>Photo: <img src="https://diagnosisdiet.com/'.$img->path.'"></p>'. "\r\n";
+
+            $message_user_contents = $message_user_contents . '<p>Credentials: '.$submittedData['credentials'].'</p>'. "\r\n";
+            $message_user_contents = $message_user_contents . '<p>Phone: '.$submittedData['phone'].'</p>'. "\r\n";
+            $message_user_contents = $message_user_contents . '<p>Internal Email: '.$submittedData['email_internal'].'</p>'. "\r\n";
+            $message_user_contents = $message_user_contents . '<p>Public Email: '.$submittedData['email_public'].'</p>'. "\r\n";;
+            $message_user_contents = $message_user_contents . '<p>Website: '.$submittedData['website'].'</p>'. "\r\n";
+            $message_user_contents = $message_user_contents . '<p>Address 1: '.$submittedData['address_1'].'</p>'. "\r\n";
+            $message_user_contents = $message_user_contents . '<p>Address 2: '.$submittedData['address_2'].'</p>'. "\r\n";
+            $message_user_contents = $message_user_contents . '<p>Country: '.$submittedData['country'].'</p>'. "\r\n";
+            
+            // if USA, add State
+            if($submittedData['country'] == 'USA')
+                $message_user_contents = $message_user_contents . '<p>State: '.$submittedData['state'].'</p>'. "\r\n";
+                
+            // if Canada, add Providence
+            if($submittedData['country'] == 'Canada')
+                $message_user_contents = $message_user_contents . '<p>Providence: '.$submittedData['providence'].'</p>'. "\r\n";
+            
+            $message_user_contents = $message_user_contents . '<p>City: '.$submittedData['city'].'</p>'. "\r\n";
+            $message_user_contents = $message_user_contents . '<p>Zip: '.$submittedData['zip'].'</p>'. "\r\n";
+            
+            if($submittedData['how_to_contact'])
+                $message_user_contents = $message_user_contents . '<p>Preferred Contact Method: '.implode(',', $submittedData['how_to_contact']).'</p>'. "\r\n\r\n";
+            
+            $message_user_contents = $message_user_contents . '<p>Service Area Worldwide: '.$service_area_worldwide.'</p>'. "\r\n";
+            $message_user_contents = $message_user_contents . '<p>Service Area Country: '.$service_area_country.'</p>'. "\r\n";
+            $message_user_contents = $message_user_contents . '<p>Service Area State: '.$service_area_state.'</p>'. "\r\n";
+            $message_user_contents = $message_user_contents . '<p>Service Area Province: '.$service_area_province.'</p>'. "\r\n\r\n";
+            
+            if($submittedData['profession'])
+                $message_user_contents = $message_user_contents . '<p>Profession: '.implode(',', $submittedData['profession']).'</p>'. "\r\n";
+            
+            $message_user_contents = $message_user_contents . '<p>Practice\'s Preferred Language: '.$submittedData['language'].'</p>'. "\r\n";
+            
+            $message_user_contents = $message_user_contents . '<p>Specialties_1: '.$submittedData['specialties_1'].'</p>'. "\r\n";
+            $message_user_contents = $message_user_contents . '<p>Specialties_2: '.$submittedData['specialties_2'].'</p>'. "\r\n";
+            $message_user_contents = $message_user_contents . '<p>Specialties_3: '.$submittedData['specialties_3'].'</p>'. "\r\n";
+            $message_user_contents = $message_user_contents . '<p>Specialties_4: '.$submittedData['specialties_4'].'</p>'. "\r\n";
+            $message_user_contents = $message_user_contents . '<p>Describe Practice: '.$submittedData['describe_practice'].'</p>'. "\r\n";
+            $message_user_contents = $message_user_contents . '<p>Offer Remote Services: '.$submittedData['remote_consultations'].'</p>'. "\r\n";
+            $message_user_contents = $message_user_contents . '<p>Offer Medication Management: '.$submittedData['provide_mms'].'</p>'. "\r\n";
+            $message_user_contents = $message_user_contents . '<p>Offer Child Services: '.$submittedData['provide_cas'].'</p>'. "\r\n";
+            $message_user_contents = $message_user_contents . '<p>Completed Training: '.$submittedData['training_program'].'</p>'. "\r\n";
+            $message_user_contents = $message_user_contents . '<p>Specific Services: '.$submittedData['specific_services'].'</p>'. "\r\n";
+
+            $to = "web@brightcloudstudio.com, suzismith@diagnosisdiet.com";
+            $subject = "[DD] New Directory Submission";
+
+            $headers = "MIME-Version: 1.0" . "\r\n";
+            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+            
+            mail($to,$subject,$message_start . $message_user_contents . $message_end, $headers);
+        }
     }
 }
