9e8c65da5433f22b7a8dcffb16c230007224e6f6
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-05-04 12:21:16 -0400

    Add English language file for tl_module.php

---
+++ src/Resources/contao/languages/en/tl_module.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Contao Directory - Users can make submissions to a directory with a module to display and filter the results.
+ *
+ * Copyright (C) 2022 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/contao-directory
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_module']['listings_customItemTpl'] 	= array('Custom listing template', 'Here you can overwrite the default listing template.');
