2ffcf4e4b63d27cca194c62bdabec0266c789dbe
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-05-04 10:20:35 -0400

    Create ProcessFormDataListener.php

---
+++ src/EventListener/ProcessFormDataListener.php
@@ -0,0 +1,29 @@
+<?php
+
+ /**
+ * @copyright  Bright Cloud Studio
+ * @author     Bright Cloud Studio
+ */
+    
+namespace BCS\ContaoSignatureFormField\EventListener;
+
+use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
+use Contao\Form;
+
+#[AsHook('processFormData')]
+class ProcessFormDataListener
+{
+    /**
+     * The Contao project root directory, injected via the constructor.
+     * In services.yaml bind "%kernel.project_dir%" to $projectDir.
+     */
+    public function __construct(private readonly string $projectDir)
+    {
+    }
+
+    public function __invoke(array &$submittedData, array $labels, array $fields, Form $form): void
+    {
+        echo "HOOKED!";
+        die();
+    }
+}
