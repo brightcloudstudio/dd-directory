e2017e5eda3de55eb02457b9356c32c815bb4280
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-05-04 11:40:01 -0400

    Add BcsDDDirectoryExtension for service loading

---
+++ src/DependencyInjection/BcsDDDirectoryExtension.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @copyright  Bright Cloud Studio
+ * @author     Bright Cloud Studio
+ */
+
+namespace Bcs\DDDirectoryBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class BcsDDDirectoryExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../Resources/config')
+        );
+
+        $loader->load('services.yaml');
+    }
+}
